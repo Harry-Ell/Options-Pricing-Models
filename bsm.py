a6df78bc6363cd9d092001094de525d9c7de4234
Shreyan2700 <165422284+Shreyan2700@users.noreply.github.com>
2024-06-26 19:38:34 +0100

    Add files via upload

---
+++ bsm.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scripy.stats import norm
+
+class BSM:
+    
+    norm = norm.cdf
+    
+    def __init__(self, stock_price, strike_price, days_to_expiry, interest_rate, volatility):
+        self.price = stock_price
+        self.strike = strike_price
+        self.dte = days_to_expiry
+        self.rate = interest_rate
+        self.vol = volatility
+      
+    def bsm_call(self):
+        ''' Computing the call option price '''
+        d1 = (np.log(self.price/self.strike) + self.dte*(self.rate + 
+                            (self.vol ** 2)/2))/(self.vol*(np.sqrt(self.dte)))
+        d2 = d1 - (self.vol*(np.sqrt(self.dte)))
+        call = self.stock * norm(d1) - norm(d2) * self.strike * np.exp(-self.rate*self.dte)
+        return call
+    
+    def bsm_put(self):
+        ''' Computing the put option price'''
+        d1 = (np.log(self.price/self.strike) + self.dte*(self.rate + 
+                            (self.vol ** 2)/2))/(self.vol*(np.sqrt(self.dte)))
+        d2 = d1 - (self.vol*(np.sqrt(self.dte)))
+        put = norm(-d2) * self.strike * np.exp(-self.rate * self.dte) - norm(-d1) * self.stock
+        return put
+        
